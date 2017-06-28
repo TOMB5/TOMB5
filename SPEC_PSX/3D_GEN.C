2e760b081be59fa31360adcc624b43bd158bda42
Gh0stBlade <misternightmare@live.com>
2017-06-28 13:26:21 +0100

    Add 3D_GEN.C

---
+++ SPEC_PSX/3D_GEN.C
@@ -0,0 +1,13 @@
+#include "3D_GEN.H"
+
+long phd_left;
+long phd_right;
+long phd_top;
+long phd_bottom;
+long phd_persp;
+long w2v_matrix[12];
+long* phd_mxptr;
+long matrix_stack[24];
+struct PHD_3DPOS viewer;
+//struct VECTOR3D CamPos;
+//struct VECTOR3D CamTgt;
