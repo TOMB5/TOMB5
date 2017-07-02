40bbc545c19cca162e1a27b678179b7d7121c720
Gh0stBlade <misternightmare@live.com>
2017-07-02 13:16:36 +0100

    Add InitialiseItemArray.

---
+++ GAME/ITEMS.C
@@ -0,0 +1,35 @@
+#include "ITEMS.H"
+
+int level_items;
+short next_item_free;
+short next_item_active;
+short GlobalPulleyFrigItem;
+struct ITEM_INFO* items;
+
+#define MAX_NUM_ITEMS (256)
+
+void InitialiseItemArray(int numitems)//418E8, 41D3C
+{
+	next_item_active = -1;
+
+	struct ITEM_INFO* item = &items[0];
+	
+	//FIXME: Ugly
+	item = (struct ITEM_INFO*)(char*)(((level_items << 3) + level_items) << 4);
+
+	next_item_free = level_items;
+
+	if ((level_items + 1) < numitems)
+	{
+		for (int i = (level_items + 1); i < MAX_NUM_ITEMS; i++, item++)
+		{
+			item->next_item = i;
+
+			//FIXME: Union flags.
+			*(int*)item->active &= -2;
+		}
+	}
+	
+	//Last
+	item->next_item = -1;
+}
