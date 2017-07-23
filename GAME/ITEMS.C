b497926749cceb0a274ead9fdf17a417217c27a2
Gh0stBlade <misternightmare@live.com>
2017-07-23 04:31:13 +0100

    Add RemoveActiveItem();

--- GAME/ITEMS.C
+++ GAME/ITEMS.C
@@ -39,7 +39,52 @@ void InitialiseItemArray(int numitems)//418E8(<), 41D3C(<)
 	item->next_item = -1;
 }
 
-void RemoveActiveItem(short item_num)
+void RemoveActiveItem(short item_num)//41E98, 422EC
 {
 	//short linknum; // $v1
+	struct ITEM_INFO* a1 = &items[item_num];
+	int v0 = a1->active;
+
+	if (a1->active != 0)
+	{
+		*(int*)a1->active &= -2;
+		v0 = *(int*) a1->active;
+
+		if (next_item_active == item_num)
+		{
+			next_item_active = items[item_num].next_active;
+			return;
+		}//0x4235C
+
+		if (next_item_active == -1)//CHECK
+		{
+			next_item_active = -1;
+			return;
+		}
+
+		struct ITEM_INFO* a2 = &items[0];
+		int a3 = -1;
+		v0 = next_item_active << 3;
+
+		while (next_item_active != -1)
+		{
+			v0 += -1;
+			v0 <<= 4;
+			struct ITEM_INFO* a11 = (struct ITEM_INFO*)(((char*) a2) + v0);
+			a11->next_active = next_item_active;
+			v0 = next_item_active << 3;
+			v0 += next_item_active;
+
+			if (next_item_active == item_num)
+			{
+				v0 <<= 4;
+				struct ITEM_INFO* v00 = (struct ITEM_INFO*)(((char*) a2) + v0);
+				v00->next_active = next_item_active;
+				return;
+			}//0x42344
+		}
+	}//0x4233C
+
+	next_item_active = -2;
+	return;
 }
