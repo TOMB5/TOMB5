7913bfbb8a715189b8f9bd2ea589f608e74cd171
Gh0stBlade <misternightmare@live.com>
2017-07-31 19:40:26 +0100

    Add AlertNearbyGuards();

--- GAME/BOX.C
+++ GAME/BOX.C
@@ -1,5 +1,7 @@
 #include "BOX.H"
 
+#include "CONTROL.H"
+#include "LOT.H"
 #include "SPECIFIC.H"
 
 int number_boxes;
@@ -9,7 +11,48 @@ short* ground_zone[5][2];
 unsigned short testclip;
 unsigned short loops;
 
-void AlertNearbyGuards(ITEM_INFO* item)
+void AlertNearbyGuards(ITEM_INFO* item)//24D20(<), 24F2C(<)
 {
-	S_Warn("[AlertNearbyGuards] - Unimplemented!\n");
+	int slot = 4;
+	struct creature_info* cinfo = baddie_slots;
+	struct ITEM_INFO* target;
+	long x = 0;
+	long y = 0;
+	long z = 0;
+	long distance = 0;
+
+	//loc_24D3C
+	for (int i = 0; i < slot; i++)
+	{
+		if (cinfo->item_num == -1)
+		{
+			continue;
+		}
+
+		target = &items[cinfo->item_num + i];
+
+		//Rooms match, alert the guards!
+		if (item->room_number == target->room_number)
+		{
+			//24DCC
+			cinfo->alerted = 1;
+			continue;
+		}
+
+		x = ((target->pos.x_pos - item->pos.x_pos) / 64) * ((target->pos.x_pos - item->pos.x_pos) / 64);
+		y = ((target->pos.y_pos - item->pos.y_pos) / 64) * ((target->pos.y_pos - item->pos.y_pos) / 64);
+		z = ((target->pos.z_pos - item->pos.z_pos) / 64) * ((target->pos.z_pos - item->pos.z_pos) / 64);
+
+		distance = x + y + z;
+
+		//Though the item is not in the same room.
+		//If the distance between them is < 0x1F40, alert the guards!
+		if (distance < 0x1F40)
+		{
+			//24DCC
+			cinfo->alerted = 1;
+		}
+	}
+
+	return;
 }
