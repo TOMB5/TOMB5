25c90f7bee359a2c99c5a032d421215599d7df3e
Gh0stBlade <misternightmare@live.com>
2017-08-22 22:31:08 +0100

    SPUSOUND.C Remove unnecessary SDK functions.
    Fix S_SetReverbType();

--- SPEC_PSXPC/SPUSOUND.C
+++ SPEC_PSXPC/SPUSOUND.C
@@ -3,7 +3,7 @@
 #include "SOUND.H"
 #include "SPECIFIC.H"
 
-#ifdef PSX
+#ifdef PSX_VERSION
 	#include <sys/types.h>
 	#include <LIBSPU.H>
 #endif
@@ -22,54 +22,6 @@ unsigned char LabFreeChannel[MAX_SOUND_SLOTS];
 int LnFreeChannels;
 static unsigned char LabSPUMallocArea[16];
 unsigned long LadwSampleAddr[256];
-
-int _spu_EVdma;
-int dword_A0F90;
-int _spu_trans_mode;
-int _spu_rev_flag;
-int _spu_rev_reserve_wa;
-int _spu_rev_offsetaddr;
-int _spu_rev_attr;
-int dword_A0FA8;
-short word_A0FAC;
-short word_A0FAE;
-int dword_A0FB0;
-int dword_A0FB4;
-int dword_A0FB8;
-int dword_A0FBC;
-int dword_A0FC0[11] =
-{
-	0xC000C000, 0x00C000C0, 0xC000C000, 0x00C000C0, 0xC000C000, 0x00C000C0, 0xC000C000, 0x00C000C0, 0xC000C000, 0x00C000C0, 0xC000C000
-};
-int dword_A13F0;
-int _spu_isCalled;
-int _spu_AllocBlockNum;
-int _spu_AllocLastNum;
-int _spu_memList;
-int _spu_rev_startaddr = 0xFFFE;
-int _spu_rev_param[18];
-int _spu_RXX = 0x1F801C00;
-int dword_A1700 = 0x1F8010C0;
-int dword_A1704 = 0x1F8010C4;
-int dword_A1708 = 0x1F8010C8;
-int dword_A170C = 0x1F8010F0;
-int dword_A1710 = 0x1F801014;
-int _spu_tsa;
-int _spu_transMode;
-int _spu_addrMode;
-int _spu_mem_mode = 2;
-int _spu_mem_mode_plus = 3;
-int _spu_mem_mode_unit = 8;
-int _spu_mem_mode_unitM = 7;
-int _spu_inTransfer = 1;
-SpuTransferCallbackProc _spu_transferCallback;
-SpuIRQCallbackProc _spu_IRQCallback;
-int dword_A173C[4] = { 0x7070707, 0x7070707, 0x7070707, 0x7070707 };
-int dword_A174C;
-int dword_A1750;
-int dword_A1754;
-int dword_A1758 = 0x1F801070;
-int dword_A1768 = 0x1F801040;
 
 void SPU_Init()//62650(<), 62D34(<) (F)
 {
@@ -108,13 +60,15 @@ void SPU_FreeChannel(int channel_index)//91668, 936AC (F)
 	LabFreeChannel[LnFreeChannels++] = channel_index;
 }
 
-void S_SetReverbType(int Reverb /*$a0*/)//91CF4, 93D40
+void S_SetReverbType(int Reverb)//91CF4, 93D40
 {
 	if (Reverb != CurrentReverb)
 	{
 		CurrentReverb = Reverb;
-		//jal	sub_6B768 // SpuSetReverbModeDepth(DepthTable[Reverb],?);
-		//jal	sub_6B850 //SpuSetReverb(1); //SPU_ON
+#ifdef PSX
+		SpuSetReverbModeDepth(DepthTable[Reverb], DepthTable[Reverb]);
+		SpuSetReverb(1); //SPU_ON
+#endif
 	}
 }
 
@@ -152,14 +106,4 @@ unsigned char SPU_AllocChannel()//915B0, 935F4
 	LnFreeChannels--;
 	
 	return LabFreeChannel[LnFreeChannels];
-}
-
-void _SpuIsInAllocateArea_(int arg0)//734D0(<), 71ABC(<) (F)
-{
-	_SpuIsInAllocateArea(arg0 << _spu_mem_mode_plus);
-}
-
-void _SpuIsInAllocateArea(int arg0)//73450, 71A3C
-{
-	S_Warn("[_SpuIsInAllocateArea] - Unimplemented!\\n");
 }
