321a68590f3f6b1dd16c36215be35e20798f6c92
Gh0stBlade <misternightmare@live.com>
2017-08-24 13:58:00 +0100

    Add SetupPadVibration

--- SPEC_PSX/PSOUTPUT.C
+++ SPEC_PSX/PSOUTPUT.C
@@ -8,37 +8,21 @@ static struct VIBRATION vib[2];
 
 void VibratePad()//604EC, 61064
 {
-#if 0
-	int i;
-	struct VIBRATION *v;
-#endif
+	return;
 }
 
-void SetupPadVibration(short num, short acc, short lev, short sus, int dec, int len)//604A4, 6101C
+void SetupPadVibration(short num, short acc, short lev, short sus, int dec, int len)//604A4(<), 6101C(<) (F)
 {
-#if 0
-	struct VIBRATION *v;
+	struct VIBRATION* v = &vib[num];
 
-	int a0;
-	a0 <<= 16;
-	a0 >>= 12;
+	v->Acc = acc;
+	v->Lev = lev;
+	v->Flag = 0;
+	v->Rate = 0;
+	v->Vib = 0;
+	v->Sus = len - dec;
+	v->Dec = dec;
+	v->Len = len;
 
-
-	struct VIBRATION* v0 = &vib[0];
-		addiu	$v0, $gp, 0x4088
-		addu	$a0, $v0
-		sll	$a3, 16
-		sra	$a3, 16
-		sh	$a1, 6($a0)
-		sh	$a2, 4($a0)
-		sh	$zero, 0xC($a0)
-		sh	$zero, 0($a0)
-		sh	$zero, 0xE($a0)
-		lh	$v1, arg_14($sp)
-		lh	$v0, arg_10($sp)
-		subu	$a3, $v1, $a3
-		sh	$a3, 0xA($a0)
-		sh	$v0, 8($a0)
-		sh	$v1, 2($a0)
-#endif
+	return;
 }
