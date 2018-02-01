bd9e80a7803d8d80cb7beb2b45ff8b202d538b7b
Gh0stBlade <misternightmare@live.com>
2018-02-01 08:58:21 +0000

    Finish ProfileAddOT()

--- SPEC_PSX/PROFILE.C
+++ SPEC_PSX/PROFILE.C
@@ -79,15 +79,12 @@ void ProfileReadCount()//61A48(<), * (F)
 #endif
 }
 
-void ProfileAddOT(unsigned long* ot)//61A90, *
+void ProfileAddOT(unsigned long* ot)//61A90, * (F)
 {
-	int count = 0;//$t0
-	long a0;
+	int count = 0;
 
-	//t3 = ot
 	if (nummarks > 0)
 	{
-		//a3 = &db;
 		//loc_61AD0
 		while (count < nummarks)
 		{
@@ -116,90 +113,35 @@ void ProfileAddOT(unsigned long* ot)//61A90, *
 			db.polyptr += 0x14;
 		}
 	}
+
 	//loc_61B78
-#if 0
-		slt	$v1, $t0, $v1
-		addiu	$v0, 0x14
-		bnez	$v1, loc_61AD0
-		
+	if (numprof > 0)
+	{
+		for (count = 0; count < numprof; count++)
+		{
+			if ((unsigned long) db.polyptr < (unsigned long) db.polybuf_limit)
+			{
+				((char*) db.polyptr)[3] = 5;
+				((char*) db.polyptr)[7] = 0x29;
+				((char*) db.polyptr)[4] = ProfileInfo[count].r;
+				((char*) db.polyptr)[5] = ProfileInfo[count].g;
+				((char*) db.polyptr)[6] = ProfileInfo[count].b;
+				((short*) db.polyptr)[10] = 0x19;
+				((short*) db.polyptr)[4] = ProfileInfo[count].profile_xcnt;
+				((short*) db.polyptr)[7] = 0x19;
+				((short*) db.polyptr)[6] = ProfileInfo[count].profile_xcnt + ProfileInfo[count].finalcnt;
+				((short*) db.polyptr)[9] = 0x21;
+				((short*) db.polyptr)[8] = ProfileInfo[count].profile_xcnt;
+				((short*) db.polyptr)[11] = 0x21;
+				((short*) db.polyptr)[10] = ProfileInfo[count].profile_xcnt + ProfileInfo[count].finalcnt;
 
-		loc_61B78:
-		lw	$v0, 0x40D4($gp)
-		nop
-		blez	$v0, locret_61C8C
-		move	$t0, $zero
-		lui	$v0, 0xA
-		addiu	$t2, $v0, 0x3BF0
-		li	$t6, 0x19
-		li	$t5, 0x21
-		li	$t1, 0xFFFFFF
-		lui	$t4, 0xFF00
-		li	$t7, 5
+				((long*) db.polyptr)[0] = (((long*) db.polyptr)[0] & 0xFF000000) | (ot[0] & 0xFFFFFF);
+				ot[0] = (ot[0] & 0xFF000000) | ((unsigned long) db.polyptr & 0xFFFFFF);
 
-		loc_61BA8:
-		lw	$a3, 8($t2)
-		lw	$v0, 0x10($t2)
-		nop
-		sltu	$v0, $a3, $v0
-		beqz	$v0, locret_61C8C
-		li	$v0, 0x29
-		sll	$a2, $t0, 3
-		addiu	$a0, $gp, 0x40D8
-		addu	$a0, $a2, $a0
-		sb	$t7, 3($a3)
-		sb	$v0, 7($a3)
-		lbu	$v0, 0($a0)
-		nop
-		sb	$v0, 4($a3)
-		lbu	$v1, 1($a0)
-		addiu	$a1, $gp, 0x40DE
-		sb	$v1, 5($a3)
-		lbu	$v0, 2($a0)
-		addu	$a1, $a2, $a1
-		sb	$v0, 6($a3)
-		addiu	$v0, $gp, 0x40DC
-		lhu	$v1, 0($a1)
-		addu	$a2, $v0
-		sh	$t6, 0xA($a3)
-		sh	$v1, 8($a3)
-		lhu	$v1, 0($a1)
-		lhu	$v0, 0($a2)
-		sh	$t6, 0xE($a3)
-		addu	$v1, $v0
-		sh	$v1, 0xC($a3)
-		lhu	$a0, 0($a1)
-		sh	$t5, 0x12($a3)
-		sh	$a0, 0x10($a3)
-		lhu	$v0, 0($a1)
-		lhu	$v1, 0($a2)
-		nop
-		addu	$v0, $v1
-		lw	$v1, 0($a3)
-		addiu	$t0, 1
-		sh	$t5, 0x16($a3)
-		sh	$v0, 0x14($a3)
-		lw	$v0, 0($t3)
-		and $v1, $t4
-		and	$v0, $t1
-		or $v1, $v0
-		and	$v0, $a3, $t1
-		sw	$v1, 0($a3)
-		lw	$a0, 0($t3)
-		lw	$v1, 0x40D4($gp)
-		and $a0, $t4
-		or $a0, $v0
-		sw	$a0, 0($t3)
-		lw	$v0, 8($t2)
-		slt	$v1, $t0, $v1
-		addiu	$v0, 0x18
-		bnez	$v1, loc_61BA8
-		sw	$v0, 8($t2)
-
-		locret_61C8C:
-	jr	$ra
-		nop
-#endif
-	S_Warn("[ProfileAddOT] - Unimplemented!\n");
+				db.polyptr += 0x18;
+			}
+		}
+	}
 }
 
 void ProfileRGB(int r, int g, int b)//61C94, * (F)
