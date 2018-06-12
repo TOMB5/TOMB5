e1d3de7edf66359e969bf4b4900073035cdf763c
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-06-12 16:40:58 +0100

    Fix PSXPC Compile.

--- SPEC_PSXPC/MATHS.C
+++ SPEC_PSXPC/MATHS.C
@@ -524,6 +524,12 @@ void phd_LookAt(long x, long y, long z, long tx, long ty, long tz, long croll)
 	S_Warn("[phd_LookAt] - Unimplemented!\n");
 }
 
+long phd_atan_asm(long x, long y)
+{
+	S_Warn("[phd_atan_asm] - Unimplemented!\n");
+	return 0;
+}
+
 #if 0
 
 0x76DB8 iRotX
