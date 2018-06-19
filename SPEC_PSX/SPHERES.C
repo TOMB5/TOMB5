06814217274dc1504385edc08bcd88b8ccde052d
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-06-19 21:27:07 +0100

    Add GetJointAbsPosition

---
+++ SPEC_PSX/SPHERES.C
@@ -0,0 +1,8 @@
+#include "SPECIFIC.H"
+
+#ifndef USE_ASM
+void GetJointAbsPosition(struct ITEM_INFO* item, struct PHD_VECTOR* pos, int joint)
+{
+	S_Warn("[GetJointAbsPosition] - Unimplemented!\n");
+}
+#endif
