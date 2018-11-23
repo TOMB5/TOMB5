75c61be39c02d7a255a9e8694342f9f7205635ca
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-11-23 11:01:53 +0000

    Remove code causing PSX regression

--- GAME/DELTAPAK.C
+++ GAME/DELTAPAK.C
@@ -37,7 +37,7 @@
 #include "ROOMLOAD.H"
 #include "MATHS.H"
 #include "DRAWPHAS.H"
-
+#include "DELTAPAK_S.H"
 #include "CD.H"
 #endif
 
@@ -3195,17 +3195,17 @@ void frigup_lara()//2D000(<), ? (F)
 	object = &objects[lara_item->object_number];
 	bone = &bones[object->bone_index];
 
-	//updateAnimFrame(&actor_pnodes[0], 0x10, frame);
+	//updateAnimFrame(actor_pnodes[0], 0x10, frame);
 	DEL_CalcLaraMatrices_Normal_ASM(frame, bone, 0);
 	mPushUnitMatrix();
 	DEL_CalcLaraMatrices_Normal_ASM(frame, bone, 1);
 	mPopMatrix();
 
-	HairControl(0, 0, frame);
+	//HairControl(0, 0, frame);
 
 	if ((gfLevelFlags & GF_LVOP_YOUNG_LARA))
 	{
-		HairControl(0, 1, frame);
+		//HairControl(0, 1, frame);
 	}
 
 	//loc_2D0F0
