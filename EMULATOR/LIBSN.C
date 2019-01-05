77c61dcb8489074cf592dc474f508d6d6aae9d10
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-01-05 22:03:12 +0000

    Stubs

---
+++ EMULATOR/LIBSN.C
@@ -0,0 +1,45 @@
+#include "LIBSN.H"
+
+#include <assert.h>
+
+int PCinit()
+{
+	assert(0);
+	return 0;
+}
+
+int PCopen(char* name, int flags, int perms)
+{
+	assert(0);
+	return 0;
+}
+
+int PCcreat(char* name, int perms)
+{
+	assert(0);
+	return 0;
+}
+
+int PClseek(int fd, int offset, int mode)
+{
+	assert(0);
+	return 0;
+}
+
+int PCread(int fd, char* buff, int len)
+{
+	assert(0);
+	return 0;
+}
+
+int PCwrite(int fd, char* buff, int len)
+{
+	assert(0);
+	return 0;
+}
+
+int PCclose(int fd)
+{
+	assert(0);
+	return 0;
+}
