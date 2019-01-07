e70e4f9db776d8e06605fa5734ec811830f11c8e
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-01-07 22:16:18 +0000

    Switch back to psylink
    slink is too unstable/buggy and not working for my overlays right now.

--- PSX_EXPORT_OVERLAYS.bat
+++ PSX_EXPORT_OVERLAYS.bat
@@ -1,5 +1,7 @@
-slink /psx /c /p /rmips=GAME/SETUP.REL @GAME/SETUP.LNK,GAME/SETUP.BIN
-slink /psx /c /p /rmips=GAME/ANDY3.REL @GAME/ANDY3.LNK,GAME/ANDY3.BIN
-slink /psx /c /p /rmips=GAME/JOBY5.REL @GAME/JOBY5.LNK,GAME/JOBY5.BIN
-
+rem slink /psx /c /p /rmips=GAME/SETUP.REL @GAME/SETUP.LNK,GAME/SETUP.BIN
+rem slink /psx /c /p /rmips=GAME/ANDY3.REL @GAME/ANDY3.LNK,GAME/ANDY3.BIN
+rem slink /psx /c /p /rmips=GAME/JOBY5.REL @GAME/JOBY5.LNK,GAME/JOBY5.BIN
+psylink /c /p /q /rmips=GAME/SETUP.REL @GAME/SETUP.LNK,GAME/SETUP.BIN
+psylink /c /p /q /rmips=GAME/ANDY3.REL @GAME/ANDY3.LNK,GAME/ANDY3.BIN
+psylink /c /p /q /rmips=GAME/JOBY5.REL @GAME/JOBY5.LNK,GAME/JOBY5.BIN
 DEL2FAB /c+ GAME/SETUP
