aa14822165f062db2edc2ed68eb8997c318c7e7a
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-01-29 16:51:39 +0000

    Update Emulator.

--- EMULATOR/LIBGPU.C
+++ EMULATOR/LIBGPU.C
@@ -1,18 +1,17 @@
 #include "LIBGPU.H"
 
-#include "EMULATOR.H"
-
 #define GL_GLEXT_PROTOTYPES 1
+#include "GL\glew.h"
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_opengl_glext.h>
-
-#include "EMULATOR_GLOBALS.H"
-
 #include <stdio.h>
 #include <cstring>
 #include <cassert>
 #include <LIBETC.H>
+
+#include "EMULATOR.H"
+#include "EMULATOR_GLOBALS.H"
 
 unsigned short vram[1024 * 512];
 DISPENV word_33BC;
