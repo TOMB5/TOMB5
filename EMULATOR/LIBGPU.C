143451a2b6ce8b1ac9bbd1428765363bf2384184
zdimension <zippedfire@free.fr>
2019-02-24 00:08:04 +0100

    hacky hack hack

--- EMULATOR/LIBGPU.C
+++ EMULATOR/LIBGPU.C
@@ -495,14 +495,16 @@ void DrawOTagEnv(u_long* p, DRAWENV* env)
 				glVertex2f(poly->x0, poly->y0);
 				glVertex2f(poly->x1, poly->y1);
 				glEnd();
-				//Hack
-				poly++;
-				poly = (LINE_G2*)((uintptr_t)poly-4);
-				glBegin(GL_LINES);
-				glVertex2f(poly->x0, poly->y0);
-				glVertex2f(poly->x1, poly->y1);
-				glEnd();
-				
+				if (getlen(pTag) == 9)
+				{
+					poly++;
+					//poly = (LINE_G2*)((uintptr_t)poly - 4);
+					glBegin(GL_LINES);
+					glColor3ubv(&poly->r0);
+					glVertex2f(poly->x0, poly->y0);
+					glVertex2f(poly->x1, poly->y1);
+					glEnd();
+				}
 				break;
 			}
 			case 0x60: // TILE
