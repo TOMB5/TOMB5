1f06f6ace1efab4c4b3cac746f0baadfe4f2b051
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-02-25 23:43:07 +0000

    Gradient lines.

--- EMULATOR/LIBGPU.C
+++ EMULATOR/LIBGPU.C
@@ -490,16 +490,19 @@ void DrawOTagEnv(u_long* p, DRAWENV* env)
 				glBindTexture(GL_TEXTURE_2D, nullWhiteTexture);
 				LINE_G2* poly = (LINE_G2*)pTag;
 				glLineWidth(1);
-				glColor3ubv(&poly->r0);
 				glBegin(GL_LINES);
+				glColor3ubv(&poly->r0);
 				glVertex2f(poly->x0, poly->y0);
+				glColor3ubv(&poly->r1);
 				glVertex2f(poly->x1, poly->y1);
 				glEnd();
 				//Hack
 				poly++;
 				poly = (LINE_G2*)((unsigned int)poly-4);
 				glBegin(GL_LINES);
+				glColor3ubv(&poly->r0);
 				glVertex2f(poly->x0, poly->y0);
+				glColor3ubv(&poly->r1);
 				glVertex2f(poly->x1, poly->y1);
 				glEnd();
 				
