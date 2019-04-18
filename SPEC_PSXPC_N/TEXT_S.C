2937c7b0281f132d2df13f2409192122987d6366
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-04-18 18:44:21 +0100

    Update TEXT_S.C
    Fix spacing issue, problem with mis-interpreted delay slot.

--- SPEC_PSXPC_N/TEXT_S.C
+++ SPEC_PSXPC_N/TEXT_S.C
@@ -287,13 +287,10 @@ void PrintString(unsigned short x, unsigned short y, unsigned char colourFlag, c
 			else if (c == 0x20)
 			{
 				//loc_8DC14
+				s2 += 6;
 				if (!(flag & 0x1000))
 				{
 					s2 += 2;
-				}
-				else
-				{
-					s2 += 6;
 				}
 				//j loc_8DD54
 			}
