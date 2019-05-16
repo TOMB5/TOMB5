31138087840c37cc4f39eb6dcd03beecef3bd5a9
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-05-16 23:41:55 +0100

    Add DoPauseMenu()

--- SPEC_PSX/SPECIFIC.C
+++ SPEC_PSX/SPECIFIC.C
@@ -27,10 +27,11 @@
 
 #include <sys\types.h>
 #include <LIBSPU.H>
+#include <STRINGS.H>
 
-static struct REQUESTER PauseReq = { 0xA4, 0x08, 0x03, 0x00, 0x05, 0x08, 0x03, 0x0A, 0x00, { 0xDE, 0xDF, 0xE0, 0x00, 0x00 } };
-static struct REQUESTER AdjustReq = { 0xE6, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,{ 0x00, 0x00, 0x00, 0x00, 0x00 } };
-static struct REQUESTER QuitReq = { 0xE2, 0x08, 0x02, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00,{ 0xA0, 0xA1, 0x00, 0x00, 0x00 } };
+static struct REQUESTER PauseReq = { STR_PAUSED,  8, 3, 0, 5, 0, 0, 0, 0,{ STR_RESUME, STR_QUIT,  STR_GAME_SETTINGS, 0, 0 } };
+static struct REQUESTER AdjustReq = { STR_USE_DIRECTIONAL_BUTTONS, 8, 0, 0, 0, 0, 0, 0, 0,{ 0, 0, 0, 0, 0 } };
+static struct REQUESTER QuitReq = { STR_CONFIRM_QUIT, 8, 2, 1, 2, 0, 0, 0, 0,{ STR_YES, STR_NO, 0, 0, 0 } };
 static struct REQUESTER StatisticsReq = { 0xB2, 0x08, 0x01, 0x01, 0x05, 0x00, 0x00, 0x00, 0x01,{ 0xEE, 0x00, 0x00, 0x00, 0x00 } };
 
 static unsigned short PadStrings[5][8] =
@@ -45,14 +46,18 @@ static unsigned short PadStrings[5][8] =
 static unsigned char PauseMenuNum;
 unsigned short AnimComp;
 short AnimatingTexturesVOffset;
-struct REQUESTER SettingsReq = { 0xE0, 0x08, 0x05, 0x00, 0x05, 0x00, 0x03, 0x03, 0x00,{ 0xE4, 0xE5, 0xE7, 0xE3, 0xEB } };
-struct REQUESTER ConfigReq = { 0xE7, 0x08, 0x03, 0x01, 0x05, 0x00, 0x00, 0x02, 0x01,{ 0xED, 0xE8, 0xE9, 0x00, 0x00 } };
+struct REQUESTER SettingsReq = { STR_GAME_SETTINGS, 8, 5, 0, 5, 0, 3, 3, 0,{ STR_SFX_VOLUME, STR_MUSIC_VOLUME, STR_CONFIGURE_CONTROLS, STR_SCREEN_ADJUST, STR_MANUAL_TARGETING } };
+struct REQUESTER ConfigReq = { STR_CONFIGURE_CONTROLS, 8, 3, 1, 5, 0, 0, 2, 1,{ STR_ELLIPSIS, STR_CONFIGURATION, STR_VIBRATION_ON, 0, 0 } };
 unsigned char SoundFXVolume;
 unsigned short nAnimTextureRanges;
 unsigned short* AnimTextureRanges;
 unsigned short nAnimUVRanges;
 int GtSFXEnabled;
 short AnimatingTexturesV[16][8][3];
+
+static int dword_A1A58[] = { 0x14, 0x15, 0x17, 0x16, 0x1E, 0x1F, 0x1C, 0x1D };
+static char byte_A1F40 = 0;
+static char byte_A1F41 = 4;
 
 void S_control_screen_position()//6068C(<), 61204(<)
 {
@@ -551,9 +556,373 @@ void DisplayConfig(int x, int y)//6080C, 61340
 		UNIMPLEMENTED();
 }
 
-int DoPauseMenu()//60F34, 61A68
+int DoPauseMenu()//60F34(<), 61A68(<) (F)
 {
-	UNIMPLEMENTED();
+	char* s;
+	long result;
+	int prompt;
+	unsigned short* vol;
+	short level;
+	/* ??? */
+	static int cursor;
+	struct REQUESTER* AutoRequester;
+	static unsigned char AutoTime;
+	static unsigned char AutoFunc;
+	/* ??? */
+
+	s = &gfStringWad[gfStringOffset[STR_CONFIGURATION]];
+
+	prompt = 0;
+	s[strlen(s) - 1] = savegame.ControlOption + 0x31;
+
+	switch (PauseMenuNum)
+	{
+	case 0:
+		//loc_60FBC
+		result = Requester(&PauseReq);
+
+		if (result)
+		{
+			QuitReq.CursorPos = 1;
+			SettingsReq.CursorPos = 0;
+
+			if (savegame.AutoTarget)
+			{
+				SettingsReq.OptionTxt[4] = STR_AUTOMATIC_TARGETING;
+			}
+			else
+			{
+				//loc_61B40
+				SettingsReq.OptionTxt[4] = STR_MANUAL_TARGETING;
+			}
+
+			//loc_61B40
+			if (savegame.VibrateOn)
+			{
+				ConfigReq.OptionTxt[2] = STR_VIBRATION_ON;
+			}
+			else
+			{
+				ConfigReq.OptionTxt[2] = STR_VIBRATION_OFF;
+			}
+
+			PauseMenuNum = result + 1;
+
+		}//loc_61B68
+		prompt = 2;
+		break;
+	case 1:
+		//loc_6103C
+		PauseMenuNum = 0;
+		PauseReq.CursorPos = 0;
+		return 1;
+		break;
+	case 2:
+		//loc_6105C
+		result = Requester(&QuitReq);
+		if (result)
+		{
+			if (result == 0x8000)
+			{
+				reset_flag = 1;
+				return 1;
+			}
+
+			PauseMenuNum = 0;
+
+		}//loc_61090
+
+		PrintString(SCREEN_WIDTH, 232, 5, &gfStringWad[gfStringOffset[STR_SELECT]], FF_CENTER);
+		break;
+	case 3:
+		//loc_610C8
+		result = Requester(&SettingsReq);
+
+		if (result == 0x8002)
+		{
+			PauseMenuNum = 20;
+			ConfigReq.CursorPos = 1;
+			prompt = 3;
+		}
+		else if (result == 0x8003)
+		{
+			PauseMenuNum = 10;
+			prompt = 3;
+		}
+		else if (result == 0x8004)
+		{
+			if (savegame.AutoTarget)
+			{
+				SettingsReq.OptionTxt[4] = STR_AUTOMATIC_TARGETING;
+			}
+			else
+			{
+				SettingsReq.OptionTxt[4] = STR_MANUAL_TARGETING;
+			}
+			//loc_61144
+			savegame.AutoTarget ^= 1;
+
+			//loc_61160
+			if (SettingsReq.CursorPos < 2)
+			{
+				if (SettingsReq.CursorPos != 0)
+				{
+					vol = &savegame.VolumeCD;
+				}
+				else
+				{
+					vol = &savegame.VolumeFX;
+				}
+
+				//loc_61198
+				level = *vol;
+				if ((input & IN_LEFT))
+				{
+					level -= 10;
+
+					if (level < 0)
+					{
+						level = 0;
+					}
+				}//loc_611C8
+
+				if ((input & IN_RIGHT))
+				{
+					level = *vol + 10;
+
+					if (level > 255)
+					{
+						level = 255;
+					}
+				}
+				//loc_611EC
+				*vol = level;
+
+				if (SettingsReq.CursorPos == 0)
+				{
+					SoundFXVolume = level;
+					SoundEffect(SFX_TICK_TOCK, NULL, 2);
+				}
+				else
+				{
+					CDDA_SetMasterVolume(level);
+				}
+			}//loc_61224
+			prompt = 3;
+		}
+		break;
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 11:
+	case 12:
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 17:
+	case 18:
+	case 19:
+		//loc_614BC
+		if (prompt != 0)
+		{
+			if (prompt == 3)
+			{
+				PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_SELECT]], FF_CENTER);
+				PrintString(496, 232, 5, &gfStringWad[gfStringOffset[STR_BACK]], FF_R_JUSTIFY);
+			}
+			else if (prompt == 2)
+			{
+				//61520
+				PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_SELECT]], FF_CENTER);
+			}
+			else
+			{
+				PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_CANCEL]], FF_CENTER);
+			}
+
+			if ((RawEdge & IN_TRIANGLE))
+			{
+				SoundEffect(SFX_MENU_CHOOSE, NULL, 2);
+				PauseMenuNum = 0;
+			}
+		}
+		break;
+	case 10:
+		//loc_6122C
+		Requester(&AdjustReq);
+
+		PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_OK]], FF_CENTER);
+		DrawLineV(16, 16, 209, 0, 0x404040, 0);
+		DrawLineV(496, 16, 209, 0, 0x404040, 0);
+		DrawLineH(16, 16, 481, 0, 0x404040, 0);
+		DrawLineH(16, 224, 481, 0, 0x404040, 0);
+
+		S_control_screen_position();
+
+		if ((RawEdge & IN_CROSS))
+		{
+			SoundEffect(SFX_MENU_CHOOSE, NULL, 2);
+		}
+
+		PauseMenuNum = 3;
+
+		break;
+	case 20:
+		//loc_612F4
+		if (DualShock)
+		{
+			ConfigReq.nOptions = 3;
+		}
+		else
+		{
+			//loc_6130C
+			ConfigReq.nOptions = 2;
+		}
+
+		if (Requester(&ConfigReq) == 0x8002)
+		{
+			if (ConfigReq.OptionTxt[2] == STR_VIBRATION_OFF)
+			{
+				savegame.VibrateOn = 1;
+				ConfigReq.OptionTxt[2] = STR_VIBRATION_ON;
+			}
+			else
+			{
+				//loc_61368
+				savegame.VibrateOn = 0;
+				ConfigReq.OptionTxt[2] = STR_VIBRATION_OFF;
+			}
+		}//lov_63170
+
+		if (ConfigReq.CursorPos == 1)
+		{
+			if ((RawEdge & IN_DPAD_LEFT) && savegame.ControlOption != 0)
+			{
+				SoundEffect(SFX_MENU_SELECT, NULL, 2);
+
+				if (savegame.ControlOption != 0)
+				{
+					savegame.ControlOption--;
+				}
+				else
+				{
+					savegame.ControlOption = 4;
+				}
+			}
+			else if ((RawEdge & IN_DPAD_RIGHT))
+			{
+				//613E4
+				if (savegame.ControlOption < 4)
+				{
+					SoundEffect(SFX_MENU_SELECT, NULL, 2);
+
+					if (savegame.ControlOption < 4)
+					{
+						savegame.ControlOption++;
+					}
+					else
+					{
+						//6142C
+						savegame.ControlOption = 0;
+					}
+				}
+			}
+		}
+		else
+		{
+			//loc_61434
+			PrintString(256, 232, 5, &gfStringWad[gfStringOffset[STR_SELECT]], FF_CENTER);
+		}
+
+		//61460
+		PrintString(496, 232, 5, &gfStringWad[gfStringOffset[STR_BACK]], FF_R_JUSTIFY);
+
+		if ((RawEdge & IN_TRIANGLE))
+		{
+			SoundEffect(SFX_MENU_CHOOSE, NULL, 0);
+			PauseMenuNum = 3;
+		}//614BC
+
+		break;
+	case 21:
+		//loc_62AE0
+		if (mcNumFiles == 0)
+		{
+			Requester(&NoGamesReq);
+		}
+		else
+		{
+			PrintString(SCREEN_WIDTH / 2, 32, 8, &gfStringWad[gfStringOffset[STR_SELECT_GAME_TO_LOAD]], FF_CENTER);
+			DisplayFiles(cursor, mcNumFiles);
+
+			if ((RawEdge & IN_DPAD_UP) && cursor != 0)
+			{
+				SoundEffect(SFX_MENU_SELECT, NULL, 0);
+				cursor--;
+			}//62B9C
+			else if ((RawEdge & IN_DPAD_DOWN) && cursor < mcNumFiles)
+			{
+				SoundEffect(SFX_MENU_SELECT, NULL, 0);
+				cursor++;
+			}//62BEC
+			else if ((RawEdge & IN_CROSS))
+			{
+				SoundEffect(SFX_MENU_CHOOSE, NULL, 0);
+
+				byte_A1F41 = 7;
+				AutoRequester = &LoadingReq;
+				AutoTime = 3;
+				AutoFunc = 11;
+				prompt = 0;
+
+				Requester(AutoRequester);
+
+				if (AutoTime == 0)
+				{
+					if (AutoFunc == 255)
+					{
+						//62DC0
+						prompt = 0;
+
+						if (prompt != 0)
+						{
+							PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_CANCEL]], FF_CENTER);
+
+							if ((RawEdge & IN_TRIANGLE))
+							{
+								byte_A1F40 = 0;
+								return -1;
+							}
+
+						}//62E14
+
+						return 1;
+					}
+				}
+				//62c6c
+			}
+		}
+		//62DC0
+		prompt = 0;
+
+		if (prompt != 0)
+		{
+			PrintString(SCREEN_WIDTH / 2, 232, 5, &gfStringWad[gfStringOffset[STR_CANCEL]], FF_CENTER);
+
+			if ((RawEdge & IN_TRIANGLE))
+			{
+				byte_A1F40 = 0;
+				return -1;
+			}
+		}
+		//62E14
+		break;
+	}
+
 	return 0;
 }
 
