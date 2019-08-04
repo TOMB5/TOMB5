0c4c4e82f1efa1a2bcd7054c63675a8b3fb6dca4
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-08-04 17:21:41 +0100

    [Specific-PSXPC_N] Implement phd_PutPolygons_normal

--- SPEC_PSXPC_N/DRAWOBJ.C
+++ SPEC_PSXPC_N/DRAWOBJ.C
@@ -6,6 +6,7 @@
 #include "LIBGPU.H"
 #include "GPU.H"
 #include "GTEREG.H"
+#include "MISC.H"
 
 char* do_the_flat_colursub_poly_quad(char* s0, int s2, int t6, int t7, int t8, int t9, int s4, int s5, int s6, int s7, int t1)
 {
@@ -377,34 +378,36 @@ void InitGT3(char* polyptr, int t6, int s4, int t2, int t7, int s5, int t3, int 
 	((int*)polyptr)[9] = t4;
 }
 
-void UnpackRGB(int fp, int* t6, int* t7, int* t8, int* at)//(F)
+
+void UnpackRGB(int* t2, int* t6, int* fp, int* t3, int* at, int* t7, int* t8)
 {
-	int t2;
-	int t3;
-
-	t2 = (*t6 >> 7) & fp;
-	t3 = (*t6 >> 10) & 0xF800;
-	*t6 = (*t6 >> 13) & 0xF8;
-
-	*t6 |= t3;
-	*t6 |= t2;
-	*at >>= 24;///@CHECKME
+	*t2 = *t6 >> 7;
+	*t2 &= *fp;
+	*t3 = *t6 >> 10;
+	*t3 &= 0xF800;
+	*t6 >>= 13;
+	*t6 &= 0xF8;
+	*t6 |= *t3;
+	*t6 |= *t2;
+	*at >>= 24;///@checkme
 	*at <<= 24;
 	*t6 |= *at;
-
-	t2 = (*t7 >> 7) & fp;
-	t3 = (*t7 >> 10) & 0xF800;
-	*t7 = (*t7 >> 13) & 0xF8;
-
-	*t7 |= t3;
-	*t7 |= t2;
-
-	t2 = (*t8 >> 7) & fp;
-	t3 = (*t8 >> 10) & 0xF800;
-	*t8 = (*t8 >> 13) & 0xF8;
-	
-	*t8 |= t3;
-	*t8 |= t2;
+	*t2 = *t7 >> 7;
+	*t2 &= *fp;
+	*t3 = *t7 >> 10;
+	*t3 &= 0xF800;
+	*t7 >>= 13;
+	*t7 &= 0xF8;
+	*t7 |= *t3;
+	*t7 |= *t2;
+	*t2 = *t8 >> 7;
+	*t2 &= *fp;
+	*t3 = *t8 >> 10;
+	*t3 &= 0xF800;
+	*t8 >>= 13;
+	*t8 &= 0xF8;
+	*t8 |= *t3;
+	*t8 |= *t2;
 }
 
 void MakeMono(unsigned short* s, unsigned char* d)//7F9E4, 81A28
@@ -604,329 +607,428 @@ void initialise_light_matrix()
 
 void phd_PutPolygons_normal(short* mesh, short clip)
 {
+	int v0;
+	int s5;
+	int s6;
+	int a1;
+	int v1;
+	int a22;
+	int* a2;
+	int sp[256];
+	int* s7;
+	int t0;
+	int t1;
+	int s4;
+	int at;
+	int* a3;
+	int* s3;
+	int t2;
+	int t3;
+	int t4;
+	int t5;
+	int t6;
+	int t7;
+	int t8;
+	int* a0;
+	char* s0;
+	char* s1;
+	int fp;
+	int gp;
+	int t9;
+
 	initialise_light_matrix();
 
-#if 0
-	loc_7EEF4:
-jal     sub_7EDAC
-nop
-lw      $v0, 8($a0)
-addi    $a0, 0xC
-move    $s5, $a1
-move    $s6, $v0
-srl     $a1, $v0, 16
-andi    $v1, $v0, 0x8000
-andi    $v0, 0xFF
-andi    $s7, $s6, 0x100
-beqz    $s7, loc_7EF28
-li      $a2, 0x7E
-li      $a2, 0x69
+	v0 = ((int*)mesh)[2];
+	mesh += 6;
+	s5 = clip;
+	s6 = v0;
+	a1 = v0 >> 16;
+	v1 = v0 & 0x8000;
+	v0 &= 0xFF;
 
-loc_7EF28:
-slt     $at, $v0, $a2
-bnez    $at, loc_7EF40
-add     $a1, $a0
-lui     $a2, 0x1E
-j       loc_7EF4C
-li      $a2, 0x1ECDC0
+	if ((s6 & 0x100) != 0)
+	{
+		a22 = 0x69;
+	}
+	else
+	{
+		a22 = 0x7E;
+	}
 
-loc_7EF40:
-beqz    $s7, loc_7EF4C
-lui     $a2, 0x1F80
-li      $a2, 0x1F8000B4
+	//loc_7EF28
+	a1 += (int)mesh;
+	if (v0 >= a22)
+	{
+		a2 = (int*)& tsv_buffer[0];
+	}
+	//loc_7EF40
+	if ((s6 & 0x100) != 0)
+	{
+		a2 = &sp[45];
+	}
+	else
+	{
+		a2 = &sp[0];
+	}
 
-loc_7EF4C:
-move    $s7, $a2
-beqz    $v0, loc_7F068
-nop
-cfc2    $t0, $7
-move    $t1, $zero
-move    $s4, $t0
-addi    $t0, -0x3000
-blez    $t0, loc_7EF74
-sll     $at, $v0, 3
-srl     $t1, $t0, 1
+	//loc_7EF4C
+	s7 = a2;
 
-loc_7EF74:
-mtc2    $t1, $8
-beqz    $v1, loc_7EFB4
-move    $v1, $v0
-move    $a3, $a0
+	if (v0 != 0)
+	{
+		t0 = TRZ;
+		t1 = 0;
+		s4 = t0;
+		t0 -= 12288;
+		at = v0 << 3;
+		if (t0 > 0)
+		{
+			t1 >>= t0;
+		}
+		//loc_7EF74
+		IR0 = t1;
 
-loc_7EF84:
-lh      $t0, 6($a3)
-addi    $a3, 8
-mtc2    $t0, $28
-addi    $v1, -1
-addi    $a2, 8
-nop
-cop2    0x680029
-mfc2    $t0, $29
-bnez    $v1, loc_7EF84
-sh      $t0, -2($a2)
-j       loc_7EFE4
-move    $a2, $s7
+		if (v1 != 0)
+		{
+			v1 = v0;
+			a3 = (int*)mesh;
 
-loc_7EFB4:
-add     $a3, $a0, $at
-move    $s3, $a3
+			//loc_7EF84
+			do
+			{
+				t0 = ((short*)a3)[3];
+				a3 += 2;
+				IRGB = t0;
+				v1--;
+				a2 += 2;
 
-loc_7EFBC:
-lwc2    $0, 0($a3)
-lwc2    $1, 4($a3)
-addi    $a3, 8
-cop2    0xE80413
-addi    $v1, -1
-addi    $a2, 8
-mfc2    $t0, $29
-bnez    $v1, loc_7EFBC
-sh      $t0, -2($a2)
-move    $a2, $s7
+				docop2(0x680029);
+				t0 = ORGB;//checkme mfc
+				((short*)a2)[-1] = t0;
+			} while (v1 != 0);
 
-loc_7EFE4:
-lw      $t0, 0($a0)
+			a2 = s7;
+		}
+		else
+		{
+			//loc_7EFB4
+			v1 = v0;
+			a3 = (int*)&mesh[at >> 1];
+			s3 = a3;
 
-loc_7EFE8:
-lw      $t1, 4($a0)
-lw      $t2, 8($a0)
+			//loc_7EFBC
+			do
+			{
+				VX0 = a3[0] & 0xFFFF;
+				VY0 = a3[0] >> 16;
+				VZ0 = a3[1];
+				a3 += 2;
+				docop2(0xE80413);
+				v1--;
+				a2 += 2;
+				t0 = ORGB;
+				((short*)a2)[-1] = t0;
+			} while (v1 != 0);
 
-loc_7EFF0:
-lw      $t3, 0xC($a0)
-lw      $t4, 0x10($a0)
-lw      $t5, 0x14($a0)
-ctc2    $a0, $21
+			a2 = s7;
+		}
+		//loc_7EFE4
+		t0 = ((int*)mesh)[0];
 
-loc_7F000:
-mtc2    $t0, $0
-mtc2    $t1, $1
-mtc2    $t2, $2
-mtc2    $t3, $3
-mtc2    $t4, $4
-mtc2    $t5, $5
-addi    $a0, 0x18
-addi    $v0, -3
-cop2    0x280030
-lw      $t0, 0($a0)
-lw      $t1, 4($a0)
-lw      $t2, 8($a0)
-lw      $t3, 0xC($a0)
-lw      $t4, 0x10($a0)
-lw      $t5, 0x14($a0)
-mfc2    $t6, $17
-mfc2    $t7, $18
-mfc2    $t8, $19
-swc2    $12, 0($a2)
-sh      $t6, 4($a2)
-swc2    $13, 8($a2)
-sh      $t7, 0xC($a2)
-swc2    $14, 0x10($a2)
-sh      $t8, 0x14($a2)
-bgtz    $v0, loc_7F000
-addi    $a2, 0x18
+		//loc_7EFE8
+		t1 = ((int*)mesh)[1];
+		t2 = ((int*)mesh)[2];
 
-loc_7F068:
-move    $a0, $s7
-lw      $a2, 0x11D4($gp)
-lw      $a3, 0x3640($gp)
-lw      $s0, 0x3644($gp)
-lw      $s1, 0x364C($gp)
-lw      $v0, 0($a1)
-lui     $fp, 0xF8
-srl     $at, $v0, 16
-ctc2    $at, $27
-andi    $at, $s6, 0xC00
-bnez    $at, loc_7F340
-andi    $s6, 0x100
-beqz    $s6, loc_7F0A8
-addi    $s4, -0xC00
-bltz    $s4, loc_7E408
-andi    $v0, 0xFFFF
+		//loc_7EFF0
+		t3 = ((int*)mesh)[3];
+		t4 = ((int*)mesh)[4];
+		t5 = ((int*)mesh)[5];
 
-loc_7F0A8:
-bltz    $s5, loc_7EA08
-andi    $v0, 0xFFFF
-lui     $gp, 0x900
-beqz    $v0, loc_7F1C4
-addi    $a1, 4
-lw      $t0, 0($a1)
+		RFC = (unsigned long)mesh;
+
+		//loc_7F000
+		do
+		{
+			VX0 = t0 & 0xFFFF;
+			VY0 = t0 >> 16;
+			VZ0 = t1;
+
+			VX1 = t2 & 0xFFFF;
+			VY1 = t2 >> 16;
+			VZ1 = t3;
+
+			VX2 = t4 & 0xFFFF;
+			VY2 = t4 >> 16;
+			VZ2 = t5;
+
+			mesh += 12;
+			v0 -= 3;
+
+			docop2(0x280030);
+
+			t0 = ((int*)mesh)[0];
+			t1 = ((int*)mesh)[1];
+			t2 = ((int*)mesh)[2];
+			t3 = ((int*)mesh)[3];
+			t4 = ((int*)mesh)[4];
+			t5 = ((int*)mesh)[5];
+
+			t6 = SZ1;
+			t7 = SZ2;
+			t8 = SZ3;
+
+			a2[0] = SXY0;
+			((short*)a2)[2] = t6;
+			a2[2] = SXY1;
+			((short*)a2)[6] = t7;
+			a2[4] = SXY2;
+			((short*)a2)[10] = t8;
+			a2 += 6;
+		} while (v0 > 0);
+	}
+	//loc_7F068
+	a0 = s7;
+	a2 = (int*)psxtextinfo;
+	a3 = (int*)db.ot;
+	s0 = db.polyptr;
+	s1 = db.polybuf_limit;
+
+	v0 = ((int*)a1)[0];
+	fp = 0xF80000;
+	at = v0 >> 16;
+	DQA = at;
+
+	at = s6 & 0xC00;
+	s6 &= 0x100;
+
+	if (at)
+	{
+		///goto DrawMesh_Env;
+	}
+
+	s4 -= 0xC00;
+	if (s6)
+	{
+		v0 &= 0xFFFF;
+		if (s4 < 0)
+		{
+			///goto DrawSubDivMesh;
+		}
+	}
+	//loc_7F0A8
+	v0 &= 0xFFFF;
+
+	if (s5 < 0)
+	{
+		///goto DrawClippedMesh;
+	}
+
+	gp = 0x9000000;
+	a1 += 4;
+
+	if (v0 != 0)
+	{
+		t0 = ((int*)a1)[0];
 
 loc_7F0C0:
-addi    $a1, 4
-li      $v1, 3
+		a1 += 4;
+		v1 = 3;
 
 loc_7F0C8:
-lw      $t1, 0($a1)
-slt     $at, $s0, $s1
-beqz    $at, loc_7F304
-addi    $v0, -1
-srl     $t8, $t1, 13
-andi    $t8, 0x7F8
-add     $t8, $a0
-srl     $t7, $t1, 5
-andi    $t7, 0x7F8
-add     $t7, $a0
-sll     $t6, $t1, 3
-andi    $t6, 0x7F8
-add     $t6, $a0
-lw      $s4, 0($t6)
-lw      $s5, 0($t7)
-lw      $s6, 0($t8)
-mtc2    $s4, $12
-mtc2    $s5, $13
-mtc2    $s6, $14
-andi    $t5, $t0, 0xFF
-srl     $t0, 8
-cop2    0x1400006
-lw      $t6, 4($t6)
-lw      $t7, 4($t7)
-lw      $t8, 4($t8)
-mtc2    $t6, $17
-mtc2    $t7, $18
-mtc2    $t8, $19
-srl     $t1, 16
-andi    $t1, 0xF00
-mfc2    $at, $24
-cop2    0x158002D
-bltz    $at, loc_7F1AC
-or      $t5, $t1
-sll     $t5, 4
-add     $t5, $a2
-mfc2    $t1, $7
-nop
-slti    $at, $t1, 0xA02
-beqz    $at, loc_7F1AC
-slti    $at, $t1, 0x21
-bnez    $at, loc_7F1AC
-sll     $t1, 2
-lw      $t4, 8($t5)
-jal     sub_7EC6C
-sll     $at, $t4, 8
-cfc2    $at, $28
-lw      $t2, 0($t5)
-lw      $t3, 4($t5)
-jal     sub_7ED50
-subu    $t2, $at
-add     $t1, $a3
-lw      $t2, 0($t1)
-sw      $s0, 0($t1)
-or      $t2, $gp
-sw      $t2, 0($s0)
-addi    $s0, 0x28
+		t1 = ((int*)a1)[0];
+		v0--;
+		if ((unsigned int)s0 < (unsigned int)s1)
+		{
+			t8 = (t1 >> 13) & 0x7F8;
+			t8 += (int)a0;
+			t7 = (t1 >> 5) & 0x7F8;
+			t7 += (int)a0;
+			t6 = (t1 << 3) & 0x7F8;
+			t6 += (int)a0;
 
-loc_7F1AC:
-beqz    $v0, loc_7F1C4
-addi    $a1, 4
-bnez    $v1, loc_7F0C8
-addi    $v1, -1
-j       loc_7F0C0
-lw      $t0, 0($a1)
+			s4 = ((int*)t6)[0];
+			s5 = ((int*)t7)[0];
+			s6 = ((int*)t8)[0];
 
-loc_7F1C4:
-cfc2    $v0, $27
-lui     $gp, 0xC00
-beqz    $v0, loc_7F304
-lw      $t0, 0($a1)
+			SXY0 = s4;
+			SXY1 = s5;
+			SXY2 = s6;
 
+			t5 = t0 & 0xFF;
+			t0 >>= 8;
+
+			docop2(0x1400006);
+
+			t6 = ((int*)t6)[1];
+			t7 = ((int*)t7)[1];
+			t8 = ((int*)t8)[1];
+
+			SZ1 = t6;
+			SZ2 = t7;
+			SZ3 = t8;
+
+			t1 >>= 16;
+			t1 &= 0xF00;
+			at = MAC0;
+			docop2(0x158002D);
+
+			t5 |= t1;
+			if (at >= 0)
+			{
+				t5 <<= 4;
+				t5 += (int)a2;
+				t1 = VZ0;
+
+				if (t1 < 0xA02 && t1 >= 0x21)
+				{
+					t1 <<= 2;
+					t4 = ((int*)t5)[2];
+					at = t4 << 8;
+
+					UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
+					at = DQB;
+					t2 = ((int*)t5)[0];
+					t3 = ((int*)t5)[1];
+					t2 -= at;
+
+					InitGT3(s0, t6, s4, t2, t7, s5, t3, t8, s6, s4);
+					t1 += (int)a3;
+					t2 = ((int*)t2)[0];
+					((int*)t1)[0] = (int)s0;
+					t2 |= gp;
+					((int*)s0)[0] = t2;
+					s0 += 0x28;
+				}//loc_7F1AC
+			}//loc_7F1AC
+
+			a1 += 4;
+			if (v0 != 0)
+			{
+				if (v1-- != 0)
+				{
+					goto loc_7F0C8;
+				}
+
+				goto loc_7F0C0;
+				t0 = ((int*)a1)[0];
+
+			}//loc_7F1C4
+		}
+		else
+		{
+			goto loc_7F304;
+		}
+	}
+	//loc_7F1C4
+	v0 = DQA;
+	gp = 0xC000000;
+	t0 = ((int*)a1)[0];
+
+	if (v0 != 0)
+	{
 loc_7F1D4:
-addi    $a1, 4
-li      $v1, 1
+		a1 += 4;
+		v1 = 1;
 
 loc_7F1DC:
-lw      $t1, 0($a1)
-slt     $at, $s0, $s1
-beqz    $at, loc_7F304
-addi    $v0, -1
-srl     $t9, $t1, 21
-srl     $t8, $t1, 13
-andi    $t8, 0x7F8
-add     $t8, $a0
-srl     $t7, $t1, 5
-andi    $t7, 0x7F8
-add     $t7, $a0
-sll     $t6, $t1, 3
-andi    $t6, 0x7F8
-add     $t6, $a0
-lw      $s4, 0($t6)
-lw      $s5, 0($t7)
-lw      $s6, 0($t8)
-mtc2    $s4, $12
-mtc2    $s5, $13
-mtc2    $s6, $14
-andi    $t9, 0x7F8
-add     $t9, $a0
-cop2    0x1400006
-lw      $t6, 4($t6)
-lw      $t7, 4($t7)
-lw      $t8, 4($t8)
-lw      $s7, 0($t9)
-lw      $t9, 4($t9)
-mtc2    $t6, $16
-mtc2    $t7, $17
-mtc2    $t8, $18
-mtc2    $t9, $19
-mfc2    $at, $24
-cop2    0x168002E
-andi    $t5, $t0, 0xFFF
-srl     $t0, 16
-bltz    $at, loc_7F2EC
-sll     $t5, 4
-add     $t5, $a2
-srl     $t2, $t9, 7
-mfc2    $t1, $7
-and     $t2, $fp
-slti    $at, $t1, 0xA02
-beqz    $at, loc_7F2EC
-slti    $at, $t1, 0x21
-bnez    $at, loc_7F2EC
-sll     $t1, 2
-srl     $t3, $t9, 10
-andi    $t3, 0xF800
-srl     $t9, 13
-andi    $t9, 0xF8
-or      $t9, $t3
-or      $t9, $t2
-lw      $t4, 8($t5)
-jal     sub_7EC6C
-move    $at, $t4
-cfc2    $at, $28
-lw      $t2, 0($t5)
-lw      $t3, 4($t5)
-lw      $t5, 0xC($t5)
-jal     sub_7ED78
-subu    $t2, $at
-add     $t1, $a3
-lw      $t2, 0($t1)
-sw      $s0, 0($t1)
-or      $t2, $gp
-sw      $t2, 0($s0)
-addi    $s0, 0x34
+		t1 = ((int*)a1)[0];
+		v0--;
 
-loc_7F2EC:
-beqz    $v0, loc_7F304
-addi    $a1, 4
-bnez    $v1, loc_7F1DC
-addi    $v1, -1
-j       loc_7F1D4
-lw      $t0, 0($a1)
+		if ((unsigned int)s0 < (unsigned int)s1)
+		{
+			t9 = (t1 >> 21) & 0x7F8;
+			t8 = (t1 >> 13) & 0x7F8;
+			t8 += (int)a0;
+			t7 = (t1 >> 15) & 0x7F8;
+			t7 += (int)a0;
+			t6 = (t1 << 3) & 0x7F8;
+			t6 += (int)a0;
 
+			s4 = ((int*)t6)[0];
+			s5 = ((int*)t7)[0];
+			s6 = ((int*)t8)[0];
+
+			SXY0 = s4;
+			SXY1 = s5;
+			SXY2 = s6;
+
+			t9 += (int)a0;
+
+			docop2(0x1400006);
+
+			t6 = ((int*)t6)[1];
+			t7 = ((int*)t7)[1];
+			t8 = ((int*)t8)[1];
+			int s77 = ((int*)t9)[0];
+			t9 = ((int*)t9)[0];
+
+			SZ0 = t6;
+			SZ1 = t7;
+			SZ2 = t8;
+			SZ3 = t9;
+			at = MAC0;
+			docop2(0x168002E);
+
+			t5 = t0 & 0xFF;
+			t0 >>= 16;
+			t5 <<= 4;
+
+			if (at >= 0)
+			{
+				t5 += (int)a2;
+				t2 = t9 >> 7;
+				t1 = OTZ;
+				t2 &= fp;
+
+				if (t1 < 0xA02 && t1 >= 0x21)
+				{
+					t1 <<= 2;
+					t3 = t9 >> 10;
+					t3 &= 0xF800;
+					t9 >>= 13;
+					t9 &= 0xF8;
+					t9 |= t3;
+					t9 |= t2;
+					t4 = ((int*)t5)[2];
+					at = MAC0;
+					UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
+					at = DQB;
+
+					t2 = ((int*)t5)[0];
+					t3 = ((int*)t5)[1];
+					t5 = ((int*)t5)[3];
+					t2 -= at;
+					InitGT4(s0, t6, s4, t2, t7, s5, t3, t8, s6, t4, t9, s77, t5);
+					t1 += (int)a3;
+					t2 = ((int*)t1)[0];
+					((int*)t1)[0] = (int)s0;
+					t2 |= gp;
+					((int*)s0)[0] = t2;
+					s0 += 0x34;
+				}
+				//loc_7F2EC
+			}
+			//loc_7F2EC
+			a1 += 4;
+			if (v0 != 0)
+			{
+				if (v1-- != 0)
+				{
+					goto loc_7F1DC;
+				}
+
+				goto loc_7F1D4;
+				t0 = ((int*)a1)[0];
+
+			}//loc_7F304
+		}//loc_7F304
+	}
 loc_7F304:
-ctc2    $zero, $21
-lw      $gp, arg_24($sp)
-lw      $ra, arg_28($sp)
-sw      $s0, 0x3644($gp)
-lw      $s0, arg_0($sp)
-lw      $s1, arg_4($sp)
-lw      $s2, arg_8($sp)
-lw      $s3, arg_C($sp)
-lw      $s4, arg_10($sp)
-lw      $s5, arg_14($sp)
-lw      $s6, arg_18($sp)
-lw      $s7, arg_1C($sp)
-lw      $fp, arg_20($sp)
-jr      $ra
-addi    $sp, 0x2C
-#endif
+	RFC = 0;
+	db.polyptr = s0;
+	return;
 
+WANK2:
 
 }
 
@@ -1130,7 +1232,7 @@ void phd_PutPolygons_pickup(short* mesh, long shade)
 				{
 					t4 = t55->u2v2pad;
 					at = t4 << 8;
-					UnpackRGB(fp, &t6, &t7, &t8, &at);
+					UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
 					t2 = t55->u0v0clut;
 					t3 = t55->u1v1tpage;
 					InitGT3(s0, t6, s4, t2, t7, s5, t3, t8, s6, t4);
@@ -1229,7 +1331,7 @@ void phd_PutPolygons_pickup(short* mesh, long shade)
 					t9 |= t2;
 					t4 = t55->u2v2pad;
 					at = t4;
-					UnpackRGB(fp, &t6, &t7, &t8, &at);
+					UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
 
 					t2 = t55->u0v0clut;
 					t3 = t55->u1v1tpage;
@@ -1292,13 +1394,13 @@ void phd_PutPolygons_seethrough(short* mesh, unsigned char shade)
 	int t9;
 	int s3;
 
+	S_MemSet((char*)&sp[0], 0, sizeof(sp));
+
 	if (shade == 0)
 	{
 		return;
 	}
 
-	//.doit
-	//s2 = shade & 0xFF
 	if (shade == 128)
 	{
 		phd_PutPolygons_normal(mesh, 1);
@@ -1334,12 +1436,11 @@ void phd_PutPolygons_seethrough(short* mesh, unsigned char shade)
 		t1 = t0 >> 1;
 	}
 	//loc_7FB84
-
 	IR0 = t1;
-	v1 = v0;
-
+	
 	if (v1 != 0)
 	{
+		v1 = v0;
 		a3 = (int*)mesh;
 
 		//loc_7FB94
@@ -1362,8 +1463,10 @@ void phd_PutPolygons_seethrough(short* mesh, unsigned char shade)
 	}
 	else
 	{
+		v1 = v0;
+
 		//loc_7FBC4
-		a3 = (int*)&mesh[v0 << 2];
+		a3 = (int*)&mesh[at >> 1];//checkme
 		
 		//loc_7FBC8
 		do
@@ -1450,107 +1553,109 @@ void phd_PutPolygons_seethrough(short* mesh, unsigned char shade)
 	{
 		t0 = a1[0];
 
-loc_7FCAC:
+	loc_7FCAC:
 		a1++;
 		v1 = 3;
 
-loc_7FCB4:
+	loc_7FCB4:
 		t1 = a1[0];
 		v0--;
-		if (s0 >= s1)
+		if ((unsigned long)s0 < (unsigned long)s1)
+		{
+
+			t8 = (t1 >> 13) & 0x7F8;
+			t8 += (int)a0;
+
+			t7 = (t1 >> 5) & 0x7F8;
+			t7 += (int)a0;
+
+			t6 = (t1 << 3) & 0x7F8;
+			t6 += (int)a0;
+
+			s4 = ((int*)t6)[0];
+			s5 = ((int*)t7)[0];
+			s6 = ((int*)t8)[0];
+
+			SXY0 = s4;
+			SXY1 = s5;
+			SXY2 = s6;
+
+			t5 = t0 & 0xFF;
+			t0 >>= 8;
+			docop2(0x1400006);
+
+			t6 = ((int*)t6)[1];//bad value, short seems fine but upper most 16 bits are wrong
+			t7 = ((int*)t7)[1];//bad value, short seems fine but upper most 16 bits are wrong
+			t8 = ((int*)t8)[1];//bad value, short seems fine but upper most 16 bits are wrong
+			SZ1 = t6;
+			SZ2 = t7;
+			SZ3 = t8;
+
+			t1 >>= 16;
+			t1 &= 0xF00;
+			at = MAC0;
+			docop2(0x158002D);
+			t5 |= t1;
+
+			if (at >= 0)
+			{
+				t5 <<= 4;
+				t5 += (int)a2;
+				t1 = OTZ;
+
+				if (t1 < 0xA02 && t1 >= 0x21)
+				{
+					t1 <<= 2;
+					t4 = ((int*)t5)[2];
+					at = t4 << 8;
+
+					UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
+
+					at = IRGB;
+					t2 = ((int*)t5)[0];
+					t3 = ((int*)t5)[1];
+					t2 -= at;
+					t1 += (int)a3;
+					at = 0xFF9FFFFF;
+					t3 &= at;
+					at = 0x200000;
+					t3 |= at;
+					at = shade & 0xFF;
+					InitGT3_V2((int*)s0, s4, &s3, t6, at, t7, t8, s5, t3, t2, s6, t4);
+
+					t2 = ((int*)t1)[0];
+					((int*)t1)[0] = (int)s0;
+					t2 |= gp;
+					((int*)s0)[0] = (int)t2;
+					s0 += 0x28;
+
+					s0 = do_the_flat_colursub_poly_tri(s0, shade & 0xFF, t6, t7, t8, s4, s5, s6, t1);
+
+				}//loc_7FDB8
+			}//loc_7FDB8
+
+			a1++;
+			if (v0 != 0)
+			{
+				if (v1-- != 0)
+				{
+					goto loc_7FCB4;
+				}
+
+				t0 = a1[0];
+				goto loc_7FCAC;
+
+			}//loc_7FDD0
+		}//loc_7FDD0
+		else
 		{
 			goto DrawExit;
 		}
 
-		t8 = (t1 >> 13) & 0x7F8;
-		t8 += (int)a0;
-
-		t7 = (t1 >> 5) & 0x7F8;
-		t7 += (int)a0;
-
-		t6 = (t1 << 3) & 0x7F8;
-		t6 = (int)a0;
-
-		s4 = ((int*)t6)[0];
-		s5 = ((int*)t7)[0];
-		s6 = ((int*)t8)[0];
-
-		SXY0 = s4;
-		SXY1 = s5;
-		SXY2 = s6;
-
-		t5 = t0 & 0xFF;
-		t0 >>= 8;
-		docop2(0x1400006);
-
-		t6 = ((int*)t6)[1];
-		t7 = ((int*)t7)[1];
-		t8 = ((int*)t8)[1];
-		SZ1 = t6;
-		SZ2 = t7;
-		SZ3 = t8;
-
-		t1 >>= 16;
-		t1 &= 0xF00;
-		at = MAC0;
-		docop2(0x158002D);
-		t5 |= t1;
-
-		if (at >= 0)
-		{
-			t5 <<= 4;
-			t5 += (int)a2;
-			t1 = OTZ;
-
-			if (t1 < 0xA02 && t1 >= 0x21)
-			{
-				t1 <<= 2;
-				t4 = ((int*)t5)[3];
-				at = t4 << 8;
-
-				UnpackRGB(fp, &t6, &t7, &t8, &at);
-
-				at = IRGB;
-				t2 = ((int*)t5)[0];
-				t3 = ((int*)t5)[1];
-				t2 -= at;
-				t1 += (int)a3;
-				at = 0xFF9FFFFF;
-				t3 &= at;
-				at = 0x200000;
-				t3 |= at;
-				at = shade & 0xFF;
-				InitGT3_V2((int*)s0, s4, &s3, t6, at, t7, t8, s5, t3, t2, s6, t4);
-
-				t2 = ((int*)t1)[0];
-				((int*)t1)[0] = (int)s0;
-				t2 |= gp;
-				((int*)s0)[0] = (int)t2;
-				s0 += 0x28;
-
-				s0 = do_the_flat_colursub_poly_tri(s0, shade & 0xFF, t6, t7, t8, s4, s5, s6, t1);
-
-			}//loc_7FDB8
-		}//loc_7FDB8
-
-		a1++;
-		if (v0 != 0)
-		{
-			if (v1-- != 0)
-			{
-				goto loc_7FCB4;
-			}
-
-			t0 = a1[0];
-			goto loc_7FCAC;
-
-		}//loc_7FDD0
-	}//loc_7FDD0
-
-	DQA = v0;
-	gp = 0xC000000;
-	t0 = a1[0];
-
+		DQA = v0;
+		gp = 0xC000000;
+		t0 = a1[0];
+	}
 	if (v0 == 0)
 	{
 		goto DrawExit;
@@ -1613,14 +1718,14 @@ loc_7FDE8:
 			{
 				t1 <<= 2;
 				t3 = t9 >> 10;
-				t3 = 0xF800;
+				t3 &= 0xF800;
 				t9 >>= 13;
 				t9 &= 0xF8;
 				t9 |= t3;
 				t9 |= t2;
 				t4 = ((int*)t5)[2];
 				at = t4;
-				UnpackRGB(fp, &t6, &t7, &t8, &at);
+				UnpackRGB(&t2, &t6, &fp, &t3, &at, &t7, &t8);
 
 				at = DQB;
 
@@ -1665,6 +1770,8 @@ loc_7FDE8:
 	{
 		goto DrawExit;
 	}
+
+	return;
 
 DrawExit:
 	RFC = 0;
