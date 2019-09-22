c8bbbc0c04106ec5fb22a6ff42981e9fb74ad1a1
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-09-22 21:31:32 +0100

    [Specific-PSXPC_N] Move PRINTOBJ functions to ANIMITEM

--- SPEC_PSXPC_N/ANIMITEM.C
+++ SPEC_PSXPC_N/ANIMITEM.C
@@ -1,5 +1,11 @@
 #include "ANIMITEM.H"
 
+#include "CONTROL.H"
+#include "LOAD_LEV.H"
+#include "DRAW.H"
+#include "CAMERA.H"
+#include "SETUP.H"
+
 #include <LIBGTE.H>
 #include "GTEREG.H"
 
@@ -270,4 +276,275 @@ int mClipBoundingBox2(short* bounds, int* sp /*fp*/)//811FC
 	}
 
 	return v0;
+}
+
+void init_scratchpad(int* fp)//8281C(<) (F)
+{
+	int t0;
+	int t1;
+	int t2;
+	int t3;
+	int t4;
+	int t5;
+	int t6;
+	int t7;
+	int* at = &fp[47];
+	fp[20] = (int)at;
+
+	t0 = R11 | (R12 << 16);
+	t1 = R13 | (R21 << 16);
+	t2 = R22 | (R23 << 16);
+	t3 = R31 | (R32 << 16);
+	t4 = R33;
+	t5 = TRX;
+	t6 = TRY;
+	t7 = TRZ;
+
+	at[0] = t0;
+	at[1] = t1;
+	at[2] = t2;
+	at[3] = t3;
+	at[4] = t4;
+	at[5] = t5;
+	at[6] = t6;
+	at[7] = t7;
+
+	fp[16] = (int)stashed_objects_list;
+	fp[17] = (int)stashed_matrix_list;
+	fp[18] = 0;
+	fp[24] = (int)items;
+	fp[38] = (int)room;
+
+	((short*)fp)[53] = camera.pos.room_number;
+	fp[39] = (int)anims;
+	fp[40] = (int)meshes;
+	fp[41] = (int)bones;
+
+	t0 = ((int*)MatrixStack)[5];
+	t1 = ((int*)MatrixStack)[6];
+	t2 = ((int*)MatrixStack)[7];
+
+	fp[46] = BinocularRange;
+	fp[27] = t0;
+	fp[28] = t1;
+	fp[29] = t2;
+
+	/* Must be set in outer function */
+	//s1 = number_draw_rooms 
+	//s0 = &draw_rooms[0];
+}
+
+
+void CalcAllAnimatingItems_ASM()
+{
+	int scratchPad[256];
+	int* fp;
+	int v1;
+	struct room_info* r;//$s2
+	struct static_info* s5;
+	int at;
+	int* a2;
+	int v0;
+	int i;//$s1
+	int j;//$s3
+	struct ITEM_INFO* item;//$s3
+	struct object_info* object;//$s6
+	int item_number;//$s2
+
+	fp = &scratchPad[0];
+	init_scratchpad(fp);
+
+	//s1 = number_draw_rooms 
+	//s0 = &draw_rooms[0];
+
+	if (number_draw_rooms > 0)
+	{
+		//loc_82658
+		for (i = 0; i < number_draw_rooms; i++)
+		{
+			//a0 = draw_rooms[0]
+			/*
+			lw      $v1, 0x80 + arg_18($fp)
+			sll     $v0, $a0, 2
+			addu    $v0, $a0
+			sll     $v0, 4
+			addu    $s2, $v1, $v0
+			*/
+			r = &room[draw_rooms[i]];
+			((short*)& fp[0])[52] = draw_rooms[i];
+			((short*)& fp[0])[16] = (int)r;
+			mmPushMatrix(fp);
+
+			//s3 = r->num_meshes
+			//s4 = r->mesh[0]
+			//v0 = ((int*)&r->left)[0];
+
+			if (r->num_meshes > 0)
+			{
+				//v1 = ((int*)&r->top)[0];
+				fp[36] = ((int*)& r->left)[0];
+				fp[37] = ((int*)& r->top)[0];
+				//loc_82698
+				for (j = 0; j < r->num_meshes; j++)
+				{
+					s5 = &static_objects[r->mesh[j].static_number];
+
+					if ((r->mesh[j].Flags) & 1)
+					{
+						mmPushMatrix(fp);///@CHECKME should be in ANIMITEM.C...
+						mTranslateAbsXYZ(r->mesh[j].x, r->mesh[j].y, r->mesh[j].z); ///@check if calling right function here
+						mRotY(r->mesh[j].y_rot);///@check if calling right function here
+						v1 = ((s5->flags) >> 2) << 10;
+						at = TRZ;
+
+						if (v1 && v1 < TRZ)
+						{
+							s5++;
+						}//loc_82704
+
+						v0 = mClipBoundingBox2(&s5->x_minp, fp);
+
+						if (v0)
+						{
+							a2 = (int*)fp[16];
+							fp[18]++;
+							((short*)a2)[0] = v0;
+							((int*)a2)[1] = (int)& r->mesh[j];///@checkme
+							((short*)a2)[1] = 0;
+							a2 += 3;
+							((int*)a2)[16] = (int)a2;///@checkme
+							v1 = s5->mesh_number;
+							v0 = fp[40];
+							stash_the_info(((int*)fp[s5->mesh_number])[0], fp);//((int*)meshes[s5->mesh_number])[0]
+}
+						//loc_8274C
+						mmPopMatrix2(fp);
+					}//loc_82750
+				}
+				//loc_8275C
+				//s2 = r->item_number
+
+				//v0 = r->item_number << 7
+				if (r->item_number != -1)
+				{
+					item_number = r->item_number;
+
+					//loc_8276C
+					do
+					{
+						item = &((struct ITEM_INFO*)fp[24])[item_number];//&items[r->item_number]
+						//v1 = r->item_number << 4	
+						//v1 = item->object_number
+						//s6 = objects
+						object = &objects[item->object_number];
+
+						//v0 = object->using_drawanimating_item
+						//a1 = item->status
+
+						if (object->using_drawanimating_item && item->status != 6)
+						{
+							//s2 = item
+							///CalcAnimatingItem_ASM();
+							//item = s2 //Maybe restore backup check if modified in func above
+						}
+						//loc_827BC
+						item_number = item->next_item;
+					} while (item->next_item != -1);
+
+				}//loc_827CC
+				mmPopMatrix2(fp);
+			}
+		}
+	}//loc_827DC
+	DrawAllAnimatingItems_ASM(fp[18]);
+}
+
+
+void DrawAllAnimatingItems_ASM(int s4)
+{
+	//s5 = ra
+	if (s4 > 0)
+	{
+		//s0 = &stashed_objects_list
+		mPushMatrix();
+		//s2 = &stashed_matrix_list
+
+		//loc_8291C
+	}
+	//loc_829E8
+
+
+#if 0
+	loc_8291C:
+	lh      s1, 2(s0)
+	lh      s3, 0(s0)
+	li      s6, 0x80
+	bnez    s1, loc_82950
+	lw      a0, 4(s0)
+	li      s1, 1
+	move    a3, a0
+	lw      a0, 0(a3)
+	lw      a1, 4(a3)
+	lw      a2, 8(a3)
+	lh      a3, 0xE(a3)
+	nop
+	jal     S_CalculateStaticMeshLight
+	addiu   ra, 0x18
+
+	loc_82950:
+	lw      a1, 8(s0)
+	lh      at, 0x30(a0)
+	li      s6, 0x80
+	nop
+	jal     CalculateObjectLighting
+	subu    s6, at
+
+	loc_82964:
+	lw      t0, 4(s2)
+	lw      t1, 8(s2)
+	lw      t2, 0xC(s2)
+	lw      t3, 0x10(s2)
+	ctc2    t0, r0
+	ctc2    t1, r1
+	ctc2    t2, r2
+	ctc2    t3, r3
+	lw      t0, 0x14(s2)
+	lw      t1, 0x18(s2)
+	lw      t2, 0x1C(s2)
+	lw      t3, 0x20(s2)
+	ctc2    t0, r4
+	ctc2    t1, r5
+	ctc2    t2, r6
+	ctc2    t3, r7
+	li      at, 0x80
+	beq     s6, at, loc_829C4
+	lw      a0, 0(s2)
+	slti    at, s6, 5
+	bnez    at, loc_829CC
+	move    a1, s6
+	jal     phd_PutPolygons_seethrough
+	addiu   ra, 8
+
+	loc_829C4:
+	jal     phd_PutPolygons
+	move    a1, s3
+
+	loc_829CC:
+	addiu   s1, -1
+	bgtz    s1, loc_82964
+	addiu   s2, 0x24
+	addiu   s4, -1
+	bgtz    s4, loc_8291C
+	addiu   s0, 0xC
+	jal     mPopMatrix
+
+	loc_829E8:
+	sw      zero, phd_left-GP_ADDR(gp)
+	li      v0, 0x1FF
+	li      at, 0xEF
+	sw      v0, phd_right-GP_ADDR(gp)
+	sw      zero, phd_top-GP_ADDR(gp)
+	jr      s5
+	sw      at, phd_bottom-GP_ADDR(gp)
+#endif
 }
