d61314b2649c1389af5702388f33cdf2704c6f30
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-09-28 03:07:11 +0100

    [Specific-PSXPC_N] Implement CalculateObjectLighting

--- SPEC_PSXPC_N/LIGHT.C
+++ SPEC_PSXPC_N/LIGHT.C
@@ -1,1 +1,34 @@
 #include "LIGHT.H"
+
+#include "MATHS.H"
+#include "LOAD_LEV.H"
+#include "GTEREG.H"
+
+void CalculateObjectLighting(struct ITEM_INFO* item/*a0*/, short* frmptr/*a1*/, struct STASHEDOBJ* sobject/*s0*/, short numnodestodraw/*s1*/, int t1)
+{
+	if (item->shade < 0)
+	{
+		R11 = 4096;
+		R12 = 0;
+		R13 = 0;
+		R21 = 0;
+		R22 = 4096;
+		R23 = 0;
+		R31 = 0;
+		R32 = 0;
+		R33 = 4096;
+		TRX = 0;
+		TRY = 0;
+		TRZ = 0;
+		Matrix++;
+		mRotYXZ(item->pos.y_rot, item->pos.x_rot, item->pos.z_rot);
+		mTranslateXYZ((frmptr[0] + frmptr[1]) >> 1, (frmptr[2] + frmptr[4]) >> 1, (frmptr[3] + frmptr[5]) >> 1);
+		mPopMatrix();
+		//S_CalculateLight(item->pos.x_pos + TRX, item->pos.y_pos + TRY, item->pos.z_pos + TRZ, item->room_number, &item->il);
+	}
+	else
+	{
+		//loc_8668C
+		//S_CalculateStaticMeshLight(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, 0x7FFF);
+	}
+}
