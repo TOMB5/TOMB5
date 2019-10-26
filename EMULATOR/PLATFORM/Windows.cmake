43e7f37182e53a74c8541659e3b29c3f84719cda
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-10-26 20:37:24 +0100

    [CMake] Correct Windows script.

--- EMULATOR/PLATFORM/Windows.cmake
+++ EMULATOR/PLATFORM/Windows.cmake
@@ -5,10 +5,9 @@ Message("Compiling for Windows!")
 #Find libs
 find_package(SDL2 REQUIRED)
 find_package(OpenGL REQUIRED)
-find_package(GLEW REQUIRED)
-
 #We want to link GLEW statically
 set(GLEW_USE_STATIC_LIBS ON)
+find_package(GLEW REQUIRED)
 
 #Setup project include directories
 include_directories(${GLEW_INCLUDE_DIR})
