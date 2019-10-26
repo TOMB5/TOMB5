9ad6d0f9777fd6a206cfcc1dd8c6dd4b4331f186
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2019-10-26 20:37:17 +0100

    [CMake] Correct Mingw script.

--- EMULATOR/PLATFORM/Mingw.cmake
+++ EMULATOR/PLATFORM/Mingw.cmake
@@ -5,6 +5,8 @@ Message("Compiling for Windows!")
 #Find libs
 find_package(SDL2 REQUIRED)
 find_package(OpenGL REQUIRED)
+#We want to link GLEW statically
+set(GLEW_USE_STATIC_LIBS ON)
 find_package(GLEW REQUIRED)
 
 #We want to link GLEW statically
