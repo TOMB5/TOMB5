f58c8894a95f94918daa24f4fec0d5b52644a24c
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2020-03-09 19:24:21 +0000

    [Specific-PSXPC_N] Implement S_SoundPlaySampleLooped.

--- SPEC_PSXPC_N/SFX.C
+++ SPEC_PSXPC_N/SFX.C
@@ -1,36 +1,55 @@
 #include "SFX.H"
 
+#include "SPECIFIC.H"
+
 void SPU_StopAll()
 {
-
+	UNIMPLEMENTED();
 }
 
 void SPU_FreeChannel(int channel_index)
 {
-
+	UNIMPLEMENTED();
 }
 
 void S_SoundStopSample(int handle)
 {
-
+	UNIMPLEMENTED();
 }
 
 void S_SetReverbType(int reverb)
 {
-
+	UNIMPLEMENTED();
 }
 
 void S_SoundStopAllSamples()
 {
-
+	UNIMPLEMENTED();
 }
 
 void SOUND_EndScene()
 {
-
+	UNIMPLEMENTED();
 }
 
 void SOUND_Stop()
 {
+	UNIMPLEMENTED();
+}
 
+int PlaySample(int a0, int a1, int a2, int a3, int arg_10, int t1, int t2)
+{
+	UNIMPLEMENTED();
+}
+
+int S_SoundPlaySampleLooped(int a0, int a1, int a2, int a3, int arg_10)
+{
+	if (GtSFXEnabled)
+	{
+		return PlaySample(arg_10, a1, a2, a3, 2, a0, a2);
+	}
+	else
+	{
+		return -3;
+	}
 }
