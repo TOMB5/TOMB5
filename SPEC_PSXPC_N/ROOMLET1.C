728eb8f491ce88c80b117f8680942b5252777a02
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2020-03-16 17:58:15 +0000

    [Specific-PSXPC_N] Correct ROOMLET1.

--- SPEC_PSXPC_N/ROOMLET1.C
+++ SPEC_PSXPC_N/ROOMLET1.C
@@ -1352,7 +1352,7 @@ loc_172C:
                     ((short*)s1)[407] = t4;
                     ((short*)s1)[408] = t5;
 
-                    a3 = (int)SubPolyGT4RL1((int*)&QuadVertTable, (int*)&s1[824], (int*)s1, (int*)a3, (int)s0, s3, fpp);
+                    a3 = (int)SubPolyGT4RL1((int*)&QuadVertTableRL1, (int*)&s1[824], (int*)s1, (int*)a3, (int)s0, s3, fpp);
                     t0 = (LB1 & 0xFFFF) | ((LB2 & 0xFFFF) << 16);
                     at = BFC;
                     t9 = DQA;
