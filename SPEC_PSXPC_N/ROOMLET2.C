a51a253832f7ea7483a6d00317688b0fd108fc29
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2020-03-16 16:01:23 +0000

    [Specific-PSXPC_N] Correct ROOMLET2.

--- SPEC_PSXPC_N/ROOMLET2.C
+++ SPEC_PSXPC_N/ROOMLET2.C
@@ -1060,7 +1060,7 @@ loc_15A8:
 
                     InitSubdivisionRL2((int*)s1, t1, s44, fpp, t5, t2, s55, gp, t6, t3, s66, s3, t7, s77);
                     s3 = 0;
-                    a3 = (int)SubPolyGT3RL2((int*)TriVertTableRL2[0], (int*)&s1[201], (int*)s1, (int*)a3, s3, fpp, (int)s0);
+                    a3 = (int)SubPolyGT3RL2((int*)&TriVertTableRL2, (int*)&s1[804], (int*)s1, (int*)a3, s3, fpp, (int)s0);
 
                     at = BFC;
                     t0 = (LB1 & 0xFFFF) | ((LB2 & 0xFFFF) << 16);
