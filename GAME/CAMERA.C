6d6cd524f8ee4c8369d94fbf13fc889efcb88ec5
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2020-03-29 17:08:48 +0100

    [Game]: Revert variable type change.
    The issue is not the type.

--- GAME/CAMERA.C
+++ GAME/CAMERA.C
@@ -959,8 +959,8 @@ void CalculateCamera()//27DA0(<), 27FAC(!)
 #if PSX_VERSION
 	struct ITEM_INFO* item;
 	short* bounds;
-	int  tilt;
-	int  change;
+	short tilt;
+	short change;
 	long shift;
 	long fixed_camera;
 	long y;
