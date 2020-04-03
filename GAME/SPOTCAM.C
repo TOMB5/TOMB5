ff7ad6077ac59e2cb4e0f1b86b9294abad78d9ec
Tom <zippedfire@free.fr>
2020-04-04 01:28:52 +0200

    [Specific-PC_N] code cleanup

--- GAME/SPOTCAM.C
+++ GAME/SPOTCAM.C
@@ -429,7 +429,7 @@ void InitialiseSpotCam(short Sequence)//37648, 37B48 (F)
 void CalculateSpotCams()//37ED0(<), 383D0(?) 
 {
 #if PC_VERSION
-	Log(LT_Enter, "CalculateSpotCams");
+	//Log(LT_Enter, "CalculateSpotCams");
 #endif
 	long cpx; // stack offset -96
 	long cpy; // stack offset -92
