49a92ba7858743ead6bea903c9cb7366fa6937dc
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2021-03-07 17:59:42 +0000

    [Emu]: LIBPRESS stubs.

---
+++ EMULATOR/LIBPRESS.C
@@ -0,0 +1,30 @@
+#include "LIBPRESS.H"
+
+#include "EMULATOR.H"
+
+void DecDCTin(u_long* buf, int mode)
+{
+	UNIMPLEMENTED();
+}
+
+void DecDCTout(u_long* buf, int size)
+{
+	UNIMPLEMENTED();
+}
+
+int DecDCToutCallback(void(*func)())
+{
+	UNIMPLEMENTED();
+	return 0;
+}
+
+void DecDCTReset(int mode)
+{
+	UNIMPLEMENTED();
+}
+
+int DecDCTvlc(u_long* bs, u_long* buf)
+{
+	UNIMPLEMENTED();
+	return 0;
+}
